8ca719a56e3fab4edcb5c85a384b78a9163f479e
user51887 <lismery014@gmail.com>
2024-07-13 23:09:24 +0100

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -75,13 +75,15 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': ['templates',
+                 os.path.join(BASE_DIR, 'tempus_dominus', 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.i18n',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
